1166184aa28f945fee82eb6794a2e5789fd7c4e2
Austin Arts <austinjarts@gmail.com>
2019-06-09 20:02:13 +0000

    connect users

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,23 +1,23 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
-def index
-  @places = Place.all .paginate(page: params[:page], per_page: 1)
-end
+  def index
+    @places = Place.all
+  end
 
-def new
-  @place = Place.new
-end
+  def new
+    @place = Place.new
+  end
 
-def create
-  Place.create(place_params)
-  redirect_to root_path
-end
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
 
-private
+  private
 
-def place_params
-  params.require(:place).permit(:name, :description, :address)
-end
-
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
 
