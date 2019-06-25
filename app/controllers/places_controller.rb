b6fcd71c6cafacf5124f539b3b9574da9709784d
Austin Arts <austinjarts@gmail.com>
2019-06-25 19:09:59 +0000

    user edit button

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,14 +10,13 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
   def show
    @place = Place.find(params[:id])
   end
-
 
   def edit
     @place = Place.find(params[:id])
