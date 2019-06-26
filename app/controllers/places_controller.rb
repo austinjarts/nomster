97c7ef1d6425f7f02128633bac9f4a60fb9cc67b
Austin Arts <austinjarts@gmail.com>
2019-06-26 21:37:42 +0000

    invalid entry

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -30,10 +34,14 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
     if @place.user != current_user
     return render plain: 'Not Allowed', status: :forbidden
-  end
+    end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
