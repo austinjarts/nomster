12d341023ea30998edb6c1f98a3ba253d0f32383
Austin Arts <austinjarts@gmail.com>
2019-06-27 19:22:12 +0000

    comments1

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,11 @@
 class Place < ApplicationRecord
 
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
+
   validates :name, presence: true
   validates :name, length: { minimum: 3 }
   validates :address, presence: true
